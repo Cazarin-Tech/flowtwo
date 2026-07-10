5b896910bf25ade0d073e4a4a79ea0995c0b6a8e
matheusjose04 <otakinhosad245@gmail.com>
2026-07-09 21:09:49 -0300

    feat(api): configura CORS seguro

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -12,7 +12,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 
 app.use(healthRoutes);
